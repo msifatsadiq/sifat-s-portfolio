18034e97f95163b6b0fbacdb9581524fc005a43d
msifatsadiq <msifatsadiq@gmail.com>
2025-02-09 15:42:03 +0600

    adding Header section,Hero Section etc

---
+++ src/sections/Header.tsx
@@ -0,0 +1,23 @@
+export const Header = () => {
+  return (
+    <div className="flex justify-center items-center mt-10 z-10 top-3">
+      <nav className="flex gap-1 p-0.5  border border-white/15 rounded-full bg-white/10 backdrop:blur">
+        <a className="nav-item" href="">
+          Home
+        </a>
+        <a className="nav-item" href="">
+          Projects
+        </a>
+        <a className="nav-item" href="">
+          About
+        </a>
+        <a
+          className="nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900"
+          href=""
+        >
+          Contact
+        </a>
+      </nav>
+    </div>
+  );
+};
---
+++ src/sections/Hero.tsx
@@ -0,0 +1,87 @@
+import Image from "next/image";
+
+import ArrowDown from "@/assets/icons/arrow-down.svg";
+import SparkleIcon from "@/assets/icons/sparkle.svg";
+import StarIcon from "@/assets/icons/star.svg";
+import grainImage from "@/assets/images/grain.jpg";
+import memojiImage from "@/assets/images/memoji-computer.png";
+
+import { HeroOrbit } from "@/components/HeroOrbit";
+
+export const HeroSection = () => {
+  return (
+    <div className="py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip">
+      <div className=" absolute inset-0 [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_70%,transparent)]">
+        <div
+          className="absolute inset-0 -z-30 opacity-5"
+          style={{
+            backgroundImage: `url(${grainImage.src})`,
+          }}
+        ></div>
+        <div className="size-[620px] hero-ring"></div>
+        <div className="size-[820px] hero-ring"></div>
+        <div className="size-[1020px] hero-ring"></div>
+        <div className="size-[1220px] hero-ring"></div>
+
+        <HeroOrbit size={800} rotation={-72}>
+          <StarIcon className="size-28 text-emerald-300" />
+        </HeroOrbit>
+        <HeroOrbit size={550} rotation={20}>
+          <StarIcon className="size-12 text-emerald-300" />
+        </HeroOrbit>
+        <HeroOrbit size={590} rotation={98}>
+          <StarIcon className="size-8 text-emerald-300" />
+        </HeroOrbit>
+        <HeroOrbit size={430} rotation={-14}>
+          <SparkleIcon className="size-8 text-emerald-300/20" />
+        </HeroOrbit>
+        <HeroOrbit size={440} rotation={79}>
+          <SparkleIcon className="size-5 text-emerald-300/20" />
+        </HeroOrbit>
+        <HeroOrbit size={530} rotation={178}>
+          <SparkleIcon className="size-10 text-emerald-300/20" />
+        </HeroOrbit>
+        <HeroOrbit size={710} rotation={144}>
+          <SparkleIcon className="size-14 text-emerald-300/20" />
+        </HeroOrbit>
+        <HeroOrbit size={720} rotation={85}>
+          <SparkleIcon className="size-34 rounded-full text-emerald-300/20" />
+        </HeroOrbit>
+      </div>
+
+      <div className="container">
+        <div className="flex flex-col items-center">
+          <Image className="size-[100px]" src={memojiImage} alt="image" />
+
+          <div className="bg-gray-950 border border-gray-800 px-4 py 1.5 inline-flex items-center gap-4 rounded-lg">
+            <div className="bg-green-500 size-2.5 rounded-full"></div>
+            <div className="text-sm font-semibold">
+              Available for new Projects
+            </div>
+          </div>
+          <div className="max-w-lg mx-auto ">
+            <h1 className="font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide">
+              Building Exceptional User Experience{" "}
+            </h1>
+            <p className="mt-4 text-center text-white/60 md:text-lg">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde
+              maxime facere quam explicabo consectetur exercitationem soluta
+              sint qui obcaecati assumenda?
+            </p>
+          </div>
+
+          <div className="flex flex-col md:flex-row justify-center items-center mt-8 gap-4">
+            <button className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl">
+              <span className="font-semibold">Explore My Work</span>
+              <ArrowDown className="size-4" />
+            </button>
+            <button className="inline-flex items-center border border-white gap-2 bg-white  text-gray-900 h-12 px-6 rounded-xl">
+              <span>👋</span>
+              <span className="font-semibold ">Lets Connect</span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
